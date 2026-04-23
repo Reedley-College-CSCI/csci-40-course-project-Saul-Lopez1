5c2417b848c053859213f901db054e8afacd4aa5
Saul Lopez <1043985@MY.SCCCD.EDU>
2026-04-22 21:51:00 -0700

    Added a prototype of a console and code to let the user input songs

--- Music_Library.cpp
+++ Music_Library.cpp
@@ -2,15 +2,16 @@
 // Author: [Saul Lopez]
 
 /* Description:
- * This program to build their own music library, with functions such as: 
- * adding, removing, viewing, searching for, and sorting songs. 
- * 
- Each song includes: artist, song, and genre. 
+ * This program to build their own music library, with functions such as:
+ * adding, removing, viewing, searching for, and sorting songs.
+ *
+ Each song includes: artist, song, and genre.
  *
 */
 
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <iomanip>
 #include <fstream>
 
@@ -35,34 +36,66 @@ private:
     string genre;
 };
 
+Song songs;
+int count = 0;
+
+void console();
+void userInputSongs();
 
 int main() {
 
-    Song songs;
-    string userInArtist;
-    string userInName;
-    string userInGenre;
-
-    cout << "Enter the artist's name: " << endl;
-    getline(cin, userInArtist);
-    songs.SetArtist(userInArtist);
-    cout << "Enter the song name: " << endl;
-    getline(cin, userInName);
-    songs.SetName(userInName);
-    cout << "Enter the genre: " << endl;
-    getline(cin, userInGenre);
-    songs.SetGenre(userInGenre);
-
-    cout << "Here is your song: " << endl;
-    songs.Print();
-
-
-
-
+    console();
 
     return 0;
 }
 
 void Song::Print() const {
     cout << artist << "     " << name << "     " << genre << endl;
+}
+
+void console() {
+    string consoleCommand;
+
+    cout << "Options: " << endl;
+    cout << "   Type '1' to add a song." << endl;
+    cout << "   Type '2' to remove a song." << endl;
+    cout << "   Type '3' to view your songs." << endl;
+    cout << "   Type '4' to search for a song." << endl;
+    cout << "   Type '5' to sort songs." << endl;
+    cout << "Enter an option: " << endl;
+    getline(cin, consoleCommand);
+
+    if (consoleCommand == "1") {
+        userInputSongs();
+    }
+}
+
+void userInputSongs() {
+    string artistName;
+    string songName;
+    string genreName;
+    const string SENTINEL = "Exit";
+
+    while (true) {
+        cout << "Enter the artist's name (type 'Exit' to stop): " << endl;
+        getline(cin, artistName);
+
+        if (artistName == SENTINEL) {
+            break;
+        }
+        songs.SetArtist(artistName);
+
+        cout << "Enter the song's name: " << endl;
+        getline(cin, songName);
+        songs.SetName(songName);
+
+        cout << "Enter the genre: " << endl;
+        getline(cin, genreName);
+        songs.SetGenre(genreName);
+
+        cout << "Here is your song: " << endl;
+        songs.Print();
+        cout << endl;
+    }
+    console();
 }
